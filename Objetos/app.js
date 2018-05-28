a05423dea2b682c0a026c175e5d0f88b25517347
Valentina <vale.gutierrezc@gmail.com>
2018-05-28 18:33:06 -0400

    name changed for folder

--- Objetos/app.js
+++ Objetos/app.js
@@ -27,4 +27,27 @@ class Numero{
         }
         return new Numero(respuesta);
     } 
+    dividir(otroNumero){
+        if(otroNumero.num == 0){
+            return new Numero(Infinity);
+        }
+        let respuesta = 0;
+        for(let resto = this.num;;respuesta++){
+            resto-= otroNumero.num;
+            if (resto < 0){
+                break;
+            }
+        }
+        return new Numero(respuesta);
+    }
 }
+let cero = new Numero(0);
+let uno = new Numero(1);
+let dos = new Numero(2);
+let tres = new Numero(3);
+
+console.assert(uno.sumar(dos).num == tres.num);
+console.assert(tres.restar(dos).num == uno.num);
+console.assert(uno.multiplicacion(dos).num == dos.num);
+console.assert(tres.multiplicacion(cero).num == cero.num);
+console.assert(tres.sumar(uno).num == num.sumar(tres).num);
