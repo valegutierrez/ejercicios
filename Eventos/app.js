29b85029d9d8a46e0e145f4ba2920c81e1dc9f7f
Valentina <vale.gutierrezc@gmail.com>
2018-06-14 18:33:10 -0400

    class progress

---
+++ Eventos/app.js
@@ -0,0 +1,7 @@
+console.log('El script se cargó');
+
+window.onload = () => {
+    console.log('Hola, la página se cargó')
+    const elBoton = document.getElementById('buttoncillo');
+    console.log('El botón es > ' +elBoton);
+}
--- Retos de Arrays/js/app.js
+++ Retos de Arrays/js/app.js
@@ -3,13 +3,11 @@ function computeAverageLengthOfWords(word1, word2) {
     let result = (word1.length + word2.length)/2;
     return result;
 }
-console.log(computeAverageLengthOfWords('code', 'programs'));
 
 // devuelve la ubicación de un entero en el array
 function getNthElement(array, n) {
     return array[n];
 }
-console.log(getNthElement([1,2,3,4], 1));
 
 // convierte el doble espaciado en uno simple
 function convertDoubleSpaceToSingle(stringToSingle) {
@@ -17,7 +15,6 @@ function convertDoubleSpaceToSingle(stringToSingle) {
     let singleSpaceString = arrayOne.join(' ');
     return singleSpaceString;
 }
-console.log(convertDoubleSpaceToSingle('string  con  doble  espacio'));
 
 // devuelve true si se cumplen las condiciones y false si no
 function areValidCredentials(name, password){
@@ -28,15 +25,14 @@ function areValidCredentials(name, password){
         return false;
     }
 }
-console.log(areValidCredentials('usua', 'contraseña'));
 
 // encuentra los dos numeros que son la suma del resultado
 function findPairForSum(array, number){
     for(i=0; i<array.length; i++){
         for (let j=i+1; j<array.length; j++){
             if(array[i]+array[j] == number){
-             let result = [array[i],array[j]];
-             return result;
+                let result = [array[i],array[j]];
+                return result;
             }
             else {
                 continue;
@@ -44,7 +40,6 @@ function findPairForSum(array, number){
         }
     }
 }
-console.log(findPairForSum([0, 8, 1, 7, 19, 12, 4], 1));
 
 // devuelve solo los números impares del array
 function filterOddElements(arr) {
@@ -56,16 +51,29 @@ function filterOddElements(arr) {
     }
     return result;
 }
-console.log(filterOddElements([5,1,2,9,4,]));
 
 // devuelve la cadena más corta dentro del array
-function findShortestWordAmongMixedElements(arr){
-    let result = '';
-    for(i=1; i<arr.length; i++){
-        if(parseInt(arr[i--]) < parseInt(arr[i])){
-            result = arr[i];
+function findShortestWordAmongMixedElements(arr) {
+    for(i=0; i<arr.length; i++){
+        for (j=i+1; j<arr.length; j++){
+            if(arr[i].length < arr[j].length && typeof(arr[i]) === 'string') {
+                return arr[i];
+            } else if (arr[j].length < arr[i].length && typeof(arr[j]) === 'string') {
+                return arr[j];
+            } else if (arr[i].length === arr[j].length && typeof(arr[i]) === 'string') {
+                return arr[i];
+            }
         }
     }
-    return result;
 }
-console.log(findShortestWordAmongMixedElements([4, 'two', 2, 'three']));
+console.log(findShortestWordAmongMixedElements([4, 3]));
+
+// devuelve la suma de todos los elementos
+function computeSumOfAllElements(arr) {
+    let output = 0;
+    for(i=0;i<arr.length;i++) {
+        output += arr[i];
+    }
+    return output;
+}
+console.log(computeSumOfAllElements([1, 2, 3]));
